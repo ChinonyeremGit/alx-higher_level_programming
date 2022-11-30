e12916b34a414b3e73505f98ade12e7017afecac
Peter Eziagor <petereziagor2000@gmail.com>
2022-11-29 20:59:23 -0800

    y

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_string = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 123:
+            upper_string += chr(ord(char) - 32)
+        else:
+            upper_string += char
+    print(upper_string)
