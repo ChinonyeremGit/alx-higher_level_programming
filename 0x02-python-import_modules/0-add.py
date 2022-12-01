c3e9ada21cd68ea47eb48042e4aa6f2b2a4f4be3
Peter Eziagor <petereziagor2000@gmail.com>
2022-12-01 22:38:03 +0100

    if __name__ added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add as add_
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_(a, b)))
+if __name__ == "__main__":
+    from add_0 import add as add_
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_(a, b)))
