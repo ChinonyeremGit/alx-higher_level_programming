b6fa94da3d68f9d4b29e67293309132d525d9da6
Peter Eziagor <petereziagor2000@gmail.com>
2022-11-30 21:51:28 -0800

    bytecode read

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+c_calculation(a, b, c):
+
+    if a < b:
+
+        return c
+
+    elif c > b:
+
+        return a + b
+
+    else:
+
+        return (a * b) - c
+# see byte code by uncommenting next two lines and running ./<filename>
+# import dis
+# print(dis.dis(magic_calculation))
