52e064a6c2588b1bc1c88546466f9d34e69daa0f
Peter Eziagor <petereziagor2000@gmail.com>
2022-12-21 14:24:10 +0100

    initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             nb_line += 1
-    except:
+    except IndexError:
         pass
-    finally: print()
+    finally:
+        print()
     return nb_line
