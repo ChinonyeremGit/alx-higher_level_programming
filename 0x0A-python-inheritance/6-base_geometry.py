8a571d811ba3a4994954d33f3e708c9bd98aef9a
Peter Eziagor <petereziagor2000@gmail.com>
2023-01-09 21:21:38 +0100

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' Module 5-base_geometry '''
+
+
+class BaseGeometry:
+    '''
+    raises an exception
+    Functions:
+        area(self)
+    '''
+
+    def area(self):
+        ''' funtion to raise an exception for now '''
+        raise Exception("area() is not implemented")
