5dfe2da7af9aba99982de160226bba1e87ba56ba
Peter Eziagor <petereziagor2000@gmail.com>
2023-01-09 20:39:00 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Module 0-lookup
+looks up for the attribute in an object
+'''
+
+
+def lookup(obj):
+    ''' returns a list of attributes of an object '''
+    return dir(obj)
